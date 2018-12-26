166a47c6aeda4ab67d392e6cffcf985b6146fba0
hikaru endo <hika.blue91@gmail.com>
2018-12-26 16:59:36 +0530

    'third'

--- seo/urls.py
+++ seo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.seo, name='seo'),
+    path('seo', views.seo1, name='seo1')
 ]
--- seo/views.py
+++ seo/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
-from urllib.request import urlopen as uReq
-import requests, json
-from bs4 import BeautifulSoup as soup
-
+import requests
+import bs4
 
 def seo(request):
-    if request.method=="POST":
-        keyword=request.POST.get('keyword')
+    return render(request, 'seo/index.html')
 
-        r = requests.get('https://www.google.co.jp/search?hl=ja&num=10&q=' + keyword)
 
-        res_google = requests.get(r)
+def seo1(request):
+    search_url_keyword='lalaland'
+    if request.POST['keyword']:
+        search_url_keyword=request.POST['keyword']
+    # if request.method=='POST':
+    #     search_url_keyword=request.POST.get('keyword')
+    # else:
+    #     search_url_keyword='lalaland'
+    #検索順位取得処理
+    if search_url_keyword and search_url_keyword.strip():
+        #Google検索の実施
+        search_url = 'https://www.google.co.jp/search?hl=ja&num=25&q=' + search_url_keyword
+        res_google = requests.get(search_url)
         res_google.raise_for_status()
-        bs4_google = soup(res_google.text, 'html.parser')
+        #BeautifulSoupで掲載サイトのURLを取得
+        bs4_google = bs4.BeautifulSoup(res_google.text, 'html.parser')
         link_google = bs4_google.select('div > h3.r > a')
-
+        
         result = []
         for i in range(len(link_google)):
             #なんか変な文字が入るので除く
@@ -23,34 +32,13 @@ def seo(request):
             if 'https://' in site_url or 'http://' in site_url:
                 #サイトの内容を解析
                 try:
-                    result.append(i+1, site_title, site_url)
-                    # print("[{}位:「{}」,URL「{}」]".format(i+1,site_title,site_url))
+                    result.append("{}位:「{}, URL「{}」\n".format(i+1,site_title,site_url))
                 except:
                     continue
+            # result.append('\n')
+
+    mapped_num = map(str, result)
+    result_string = ' '.join(mapped_num)
                 
-        return render(request, 'seo/index.html', {'result':result})
+    return render(request, 'seo/seo.html', {'result':result_string})
 
-    else:
-            keyword='keyword'
-
-            r = requests.get('https://www.google.co.jp/search?hl=ja&num=10&q=' + keyword)
-
-            res_google = requests.get(r)
-            res_google.raise_for_status()
-            bs4_google = soup(res_google.text, 'html.parser')
-            link_google = bs4_google.select('div > h3.r > a')
-
-            result = []
-            for i in range(len(link_google)):
-                #なんか変な文字が入るので除く
-                site_url = link_google[i].get('href').split('&sa=U&')[0].replace('/url?q=', '')
-                site_title=bs4_google.select('div > h3.r > a')[i].text#textで中身抽出。stringでもいいけど今回はnoneが返る
-                if 'https://' in site_url or 'http://' in site_url:
-                    #サイトの内容を解析
-                    try:
-                        result.append(i+1, site_title, site_url)
-                        # print("[{}位:「{}」,URL「{}」]".format(i+1,site_title,site_url))
-                    except:
-                        continue
-                    
-            return render(request, 'seo/index.html', {'result':result})
